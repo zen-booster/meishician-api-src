1be11d0ce0d581ff3bdd103b5155d1049e6ce172
Alex Lau <alex.wanna.nirvana@gmail.com>
2022-12-02 17:46:42 +0800

    fix: 修正 request檔案大小限制

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ require('./connections/passport');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
